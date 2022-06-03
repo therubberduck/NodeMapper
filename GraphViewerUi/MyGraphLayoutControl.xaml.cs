0a24f16cb073cdf1bbc922e011889b444d0bb4c8
TheRubberduck <therubberduckmail@gmail.com>
2022-06-03 12:31:04 +0200

    Initial commit

---
+++ GraphViewerUi/MyGraphLayoutControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.WpfGraphControl;
+
+namespace NodeMapper.GraphViewerUi
+{
+    public partial class MyGraphLayoutControl {
+        public GraphViewer GraphViewer;
+        
+        public MyGraphLayoutControl() {
+            InitializeComponent();
+            Loaded += (s, e) => SetGraph();
+        }
+        public Graph Graph {
+            get => (Graph)GetValue(GraphProperty);
+            set => SetValue(GraphProperty, value);
+        }
+        public static readonly DependencyProperty GraphProperty =
+            DependencyProperty.Register("Graph", typeof(Graph), typeof(MyGraphLayoutControl), new PropertyMetadata(default(Graph),
+                (d,e)=> ((MyGraphLayoutControl)d)?.SetGraph()));
+     
+        private void SetGraph() {
+            if (Graph == null) {
+                dockPanel.Children.Clear();
+                GraphViewer = null;
+                return;
+            }
+            if (GraphViewer == null) {
+                GraphViewer = new GraphViewer();
+                GraphViewer.BindToPanel(dockPanel);
+            }
+            GraphViewer.Graph = Graph;
+        }
+    }
+}
